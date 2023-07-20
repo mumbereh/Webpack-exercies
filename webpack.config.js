6b29624da2ef1a750d7aeec6209cf6f97942e562
mumbereh <siirasonh@gmail.com>
2023-07-20 09:58:45 +0300

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
 module.exports = {
     entry: "./app/index.js",
-    module:{
-       rule: [
-        {
-            test: /\.svg$/,
-            use: 'svg-inline-loader'
-        },
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test:/\.(js)$/,
-            use: "babel-loader",
-          }
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: 'svg-inline-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(js)$/,
+                use: "babel-loader",
+            }
         ],
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, 'dist'),
         filename: "style.css"
     },
-    plugins: [
-        new HtmlWebpackPlugin()
-      ]
+    plugins: [new HtmlWebpackPlugin()],
+    mode: process.env.NODE_ENV === "production" ? "production" : "development", // Fix 'prduction' to 'production'
 };
